c5627dc79a591db9e643531970f604d9096a9b83
Paul Pritchard <paulp@computerspecialists.co.za>
2021-01-22 10:14:51 +0200

    Updated About component

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ class About extends React.Component {
     render() {
         return (
             <div id="about">
-                <a id="about-link" href="https://docs.google.com/document/d/1ZsHAiDKQj5IoYlUVucCQ3IYjSFkDj8k4PAfMxSUZB-U/edit?usp=sharing" target="_blank">?</a>
+                <a id="about-link" href="https://docs.google.com/document/d/1LzbsAVsZzNF9oJ38EVFj1Cptf1zpKm_bE0JfOWHK-t8/edit?usp=sharing" target="_blank">?</a>
             </div>
         )
      } 
